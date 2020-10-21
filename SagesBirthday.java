8ab03a5d74f2ec4061f8357a97c5ccf1ec4d5448
Tom Tobiasson <tobiassontom@outlook.com>
2020-10-21 11:28:10 +0200

    10-21

---
+++ SagesBirthday.java
@@ -0,0 +1,11 @@
+public class SagesBirthday {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cakes = sc.nextInt();
+
+        for(int i = 0;i < cakes; i++){
+            
+        }
+    }
+}
