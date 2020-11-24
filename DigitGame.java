08afa96653245fd751511c50571e2d3cd0e6dbd6
ttobiasson <tobiassontom@outlook.com>
2020-11-24 10:39:16 +0100

    new solution

--- DigitGame.java
+++ DigitGame.java
@@ -4,17 +4,34 @@ public class DigitGame {
     public static void main(String[] args){
         Scanner sc =  new Scanner(System.in);
         int t = sc.nextInt();
-        
-        for(int i = 0; i < t; i++){
+        String[] strings = new String[]{"0","1","2","3","4","5","6","7","8","9"};
+
+
+        for (int k = 0; k < t; k++){
+            boolean hasOdd = false;
+            boolean hasEven = false;
             int digits = sc.nextInt();
             String n = sc.next();
+
+            for (String i : strings){
+                if (n.indexOf(i) != -1 && Integer.parseInt(i) % 2 != 0)
+                    hasOdd = true;
+                if (n.indexOf(i) != -1 && Integer.parseInt(i) % 2 == 0)
+                    hasEven = true;
+            }
+            if (digits % 2 == 0){
+                if(hasEven)
+                    System.out.println(2);
+                else
+                    System.out.println(1);
+            }
+            else {
+                if(hasOdd)
+                    System.out.println(1);
+                else
+                    System.out.println(2);
+            }
             
-            if(digits < 2 && Integer.parseInt(n) % 2 == 0)
-                System.out.println(2);
-            else if(digits % 2 == 0)
-                System.out.println(2);
-            else
-                System.out.println(1);   
         }
     }
 }
---
+++ NumberOfApartments.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class NumberOfApartments {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tests = sc.nextInt();
+
+        while (tests-- > 0){
+            int windows = sc.nextInt();
+            int sevens = 0;
+            int fives = 0;
+            int threes = 0;
+            int acc = 0;
+
+            if  (windows >= 3 && windows != 4){
+                while (windows > 0){                    
+                    if (windows % 3 == 0){
+                        threes++;
+                        windows -= 3;
+                    }
+                    else if (windows % 5 == 0){
+                        fives++;
+                        windows -= 5;
+                    }
+                    else if (windows % 7 == 0){
+                        sevens++;
+                        windows -= 7;
+                    }
+                    else{
+                        acc++;
+                        windows--;
+                    }
+
+                }
+
+                while (acc-- > 0){
+                    if (threes > 1){
+                        threes -= 2;
+                        sevens++;
+                    }
+                    else if (fives > 0){
+                        fives--;
+                        threes += 2;
+                    }
+                    else if (sevens > 0){
+                        fives++;
+                        threes++;
+                        sevens--;
+                    }
+                }
+                
+                System.out.println(threes + " " + fives + " " + sevens);
+            }
+            else {
+                System.out.println(-1);
+            }
+
+        }
+    }
+}
